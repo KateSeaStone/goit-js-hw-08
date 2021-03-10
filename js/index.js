9b436b262ff16b584a6d5d2b718e824c1d79edb0
Kate <kamennaya.kate@gmail.com>
2021-03-10 13:48:25 +0200

    export&import images

--- js/index.js
+++ js/index.js
@@ -1,70 +1,27 @@
 
-//import images from "../gallery-items"
-const images = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
+import { images } from "../gallery-items.js";
+
+/*Для коректной работы локальной страницы, необходимо исправить пути
+<link rel="stylesheet" href="css/styles.css" />
+<script src="gallery-items.js" type="module"></script>
+<script src="js/task.js" type="module"></script>
+
+Для использования данных из модуля gallery-items.js необходимо експортировать их из модуля
+export const images = [
+
+и импортировать по месту использования (в task.js )
+import { images } from "../gallery-items.js";
+
+Тепер нет необходимости в дублировании переменной images в task.js (удалите ее)
+
+Не давайте функциям названия, которые вводят в заблуждение.
+Ведь функция isOpenModal на самом деле возвращает true если окно закрыто.
+
+Измените или название этой функции,
+или ее содержимоє
+(на return refs.modal.classList.contains('is-open') и тогда проверяйте if (!isOpenModal()) return
+
+  */
 
 const refs = {
   gallery: document.querySelector('ul.gallery'),
@@ -119,6 +76,8 @@ function onOverlayClick(event) {
 
 function isOpenModal() {
   return !refs.modal.classList.contains('is-open')
+  //return refs.modal.classList.contains('is-open') и тогда проверяйте if (!isOpenModal()) return
+
 }
 
 function showNextImage(direction) {
